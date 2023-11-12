00d319e048b617505e3d2d5aaf2403539953fa2c
leonardo pereira <pereiraleo01@gmail.com>
2023-11-12 19:56:39 -0300

    primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/body/body.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+
+//Interface do meu produto e os campos que eu vou mostrar.
+interface Product {
+  id: number;
+  title: string;
+  price: number;
+  brand: string;
+}
+
+@Component({
+  selector: 'app-body',
+  templateUrl: './body.component.html',
+  styleUrls: ['./body.component.css']
+})
+export class BodyComponent implements OnInit {
+  displayedColumns: string[] = ['id', 'title', 'price', 'brand'];//Configurando como vai ser mostrado os meus campos
+  product: Product[] = [];
+  dataSource = new MatTableDataSource<Product>();//Cria minha tabela do produto
+
+  @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchData();
+  }
+
+  //Fazendo o get de acordo como estava instruido na API
+  fetchData() {
+    this.http.get<{ products: Product[]}>('https://dummyjson.com/products?limit=50').subscribe(
+      (response) => {
+        /*
+          Aqui eu pego o objeto de produto e pego os respectivos produtos e jogo para meu dataSource para criar uma Tabela que o MatTable
+          seja compativel.       
+        */
+        this.product = response.products;
+        this.dataSource = new MatTableDataSource(this.product);
+        this.dataSource.paginator = this.paginator; //paginação
+        console.log(this.dataSource) //usado para ver como esta meu dataSource
+      },
+      (error) => {
+        console.error('Erro ao buscar dados:', error);
+      });
+  }
+}
