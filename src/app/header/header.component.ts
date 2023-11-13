f8e19d2fc22ac68a9b3a0133ec5280c51de14639
leonardo pereira <pereiraleo01@gmail.com>
2023-11-13 16:53:04 -0300

    Criando e estilizando o Header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +9,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  faInstagram = faInstagram;
+  faGithub = faGithub;
+  faLinkedin = faLinkedin;
+  isMouseOverGithub = false;
+  isMouseOverLinkedin = false
+  isMouseOverInstagram = false;
 
+    onGithubMouseEnter() {
+      this.isMouseOverGithub = true;
+    }
+    onGithubMouseLeave() {
+      this.isMouseOverGithub = false;
+    }
+
+    onLinkedinMouseEnter() {
+      this.isMouseOverLinkedin = true;
+    }
+    onLinkedinMouseLeave() {
+      this.isMouseOverLinkedin = false;
+    }
+
+    onInstagramMouseEnter() {
+      this.isMouseOverInstagram = true;
+    }
+    onInstagramMouseLeave() {
+      this.isMouseOverInstagram = false;
+    }
+  
 }
